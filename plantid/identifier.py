489f128cb84ba434b57c0ca7c2ff938428b1a980
quarrying <quarrying@qq.com>
2021-06-23 23:49:09 +0800

    add PlantIdentifier.predict, and use it in PlantIdentifier.identify

--- plantid/identifier.py
+++ plantid/identifier.py
@@ -60,20 +60,38 @@ class PlantIdentifier(object):
         
     @staticmethod
     def _preprocess(image):
+        image = utils.normalize_image_shape(image)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        image = utils.resize_image_short(image, 320)
+        image = utils.center_crop(image, 299, 299)
+        image = image.astype(np.float32)
+        image /= 255.0
+        image -= np.asarray([0.485, 0.456, 0.406])
+        image /= np.asarray([0.229, 0.224, 0.225])
+        image = np.transpose(image, (2,0,1))
+        image = np.expand_dims(image, axis=0)
+        return image
+        
+    def predict(self, image):
         try:
-            image = utils.normalize_image_shape(image)
-            image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-            image = utils.resize_image_short(image, 320)
-            image = utils.center_crop(image, 299, 299)
-            image = image.astype(np.float32)
-            image /= 255.0
-            image -= np.asarray([0.485, 0.456, 0.406])
-            image /= np.asarray([0.229, 0.224, 0.225])
-            image = np.transpose(image, (2,0,1))
-            image = np.expand_dims(image, axis=0)
-            return image
+            inputs = self._preprocess(image)
         except:
-            return None
+            return {"status": -1, "message": "Inference preprocess error.", "results": {}}
+        
+        try:
+            self.net.setInput(inputs)
+            logits = self.net.forward()
+            probs = utils.softmax(logits)
+        except:
+            return {"status": -2, "message": "Inference error.", "results": {}}
+            
+        names = [self.label_name_dict[i]['chinese_name'] for i in range(len(self.label_name_dict))]
+        family_probs, family_names = self._get_collective_probs(probs, self.family_dict)
+        genus_probs, genus_names = self._get_collective_probs(probs, self.genus_dict)
+        results = {'probs': probs, 'names': names, 
+                   'family_probs': family_probs, 'family_names': family_names, 
+                   'genus_probs': genus_probs, 'genus_names': genus_names}
+        return {"status": 0, "message": "OK", "results": results}
         
     def identify(self, image, topk=5):
         assert isinstance(topk, int)
@@ -82,22 +100,21 @@ class PlantIdentifier(object):
                        len(self.family_dict),
                        len(self.genus_dict))
         results, family_results, genus_results = [], [], []
-            
-        inputs = self._preprocess(image)
-        if inputs is None:
-            return {"status": -1, "message": "Inference preprocess error.", 
+        
+        outputs = self.predict(image)
+        status = outputs['status']
+        message = outputs['message']
+        if outputs['status'] != 0:
+            return {"status": status, "message": message, 
                     "results": results, "family_results": family_results,
                     "genus_results": genus_results}
+                    
+        probs = outputs['results']['probs']
+        family_probs = outputs['results']['family_probs']
+        family_names = outputs['results']['family_names']
+        genus_probs = outputs['results']['genus_probs']
+        genus_names = outputs['results']['genus_names']
         
-        try:
-            self.net.setInput(inputs)
-            logits = self.net.forward()
-            probs = utils.softmax(logits)
-        except:
-             return {"status": -2, "message": "Inference error.", 
-                     "results": results, "family_results": family_results,
-                     "genus_results": genus_results}
-                     
         taxon_topk = min(probs.shape[-1], topk)
         topk_probs, topk_indices = utils.find_topk(probs, taxon_topk)
         for ind, prob in zip(topk_indices[0], topk_probs[0]):
@@ -105,7 +122,6 @@ class PlantIdentifier(object):
             one_result['probability'] = prob
             results.append(one_result)
 
-        family_probs, family_names = self._get_collective_probs(probs, self.family_dict)
         family_topk = min(family_probs.shape[-1], topk)
         family_topk_probs, family_topk_indices = utils.find_topk(family_probs, family_topk)
         for ind, prob in zip(family_topk_indices[0], family_topk_probs[0]):
@@ -115,7 +131,6 @@ class PlantIdentifier(object):
             one_result['probability'] = prob
             family_results.append(one_result)
             
-        genus_probs, genus_names = self._get_collective_probs(probs, self.genus_dict)
         genus_topk = min(genus_probs.shape[-1], topk)
         genus_topk_probs, genus_topk_indices = utils.find_topk(genus_probs, genus_topk)
         for ind, prob in zip(genus_topk_indices[0], genus_topk_probs[0]):
@@ -125,7 +140,7 @@ class PlantIdentifier(object):
             one_result['probability'] = prob
             genus_results.append(one_result)
             
-        return {"status": 0, "message": "OK", 
+        return {"status": status, "message": message, 
                 "results": results, "family_results": family_results,
                 "genus_results": genus_results}
                 
